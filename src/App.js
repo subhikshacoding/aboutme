d34cd9de3263380055f1295f6b6ebb3dac01938b
subhikshacoding <subhisubhiksha31@gmail.com>
2024-03-14 18:13:51 +0530

    updating portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Home from "./routes/Home"
+import About from "./routes/About"
+import Project from "./routes/project";
+import Contact from "./routes/contact";
+import {Route,Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+       <Route path="/project" element={<Project />} />
+      <Route path="/contact" element={<Contact />} />
+
+     
+      
+     </Routes>
+
     </div>
-  );
+  )
 }
 
 export default App;
